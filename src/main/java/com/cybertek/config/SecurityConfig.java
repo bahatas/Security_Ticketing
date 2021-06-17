ad9a3c1e8be0eeafa98e67b669de7900c7dd4291
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-17 09:34:31 +0100

    security

--- src/main/java/com/cybertek/config/SecurityConfig.java
+++ src/main/java/com/cybertek/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cybertek.config;
 
 
+import com.cybertek.service.SecurityService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,15 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private SecurityService securityService;
+    private AuthSuccessHandler authSuccessHandler;
+
+
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
+        this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/welcome")
+                //.defaultSuccessUrl("/welcome")
+                .successHandler(authSuccessHandler)
                 .permitAll()
                 .and()
                 .logout()
--- src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
+++ src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
@@ -4,11 +4,15 @@ package com.cybertek.implementation;
 import com.cybertek.entity.User;
 import com.cybertek.entity.common.UserPrincipal;
 import com.cybertek.repository.UserRepository;
+import com.cybertek.service.SecurityService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class SecurityServiceImpl implements UserDetailsService {
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
     private UserRepository userRepository;
 
     public SecurityServiceImpl(UserRepository userRepository) {
