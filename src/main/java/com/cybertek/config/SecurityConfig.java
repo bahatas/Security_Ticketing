0c255ab4933b3aa9a501bb6d9910731cd4140d70
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-17 09:11:59 +0100

    security

--- src/main/java/com/cybertek/config/SecurityConfig.java
+++ src/main/java/com/cybertek/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package com.cybertek.config;
 
 
-import org.aspectj.lang.annotation.DeclareAnnotation;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/login")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout" ))
+                    .logoutSuccessUrl("/login")
                 .and()
                 .rememberMe()
                 .tokenValiditySeconds(120)
